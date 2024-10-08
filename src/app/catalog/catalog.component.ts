0c8be6a54a9a381a2d83f07e391a80d7b97629d3
Francisco Arena <francisco.arena@parvis.it>
2024-10-08 15:01:12 +0200

    Added Product and Cart Service

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
 import { productsArray } from './products-data';
+import { ProductsService } from './product.service';
+import { CartService } from '@core/cart.service';
 
 @Component({
   selector: 'bot-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css'],
 })
-export class CatalogComponent {
-  products: Product[] = productsArray;
-  private cart: Product[] = [];
+export class CatalogComponent implements OnInit{
+  products: Product[] = [];
+  
+  constructor(private productsService: ProductsService, private cartService: CartService) { 
+    //It's a best pratice in Angular to avoid using constructor as much as possible
+    //One reason it's because is more difficult to test
+    //we use instead OnInit
+  }
 
-  constructor() { }
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+  }
 
   addToCart(product: Product) {
-    this.cart.push(product);
+    this.cartService.add(product);
   }
 }
